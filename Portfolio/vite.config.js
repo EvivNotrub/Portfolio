762f7b7c15f7b7fe920f452c37b4a76e1985a339
EvivNotrub <barthwerle@yahoo.de>
2023-11-25 00:26:44 +0100

    setup for gh-pages deployment

--- Portfolio/vite.config.js
+++ Portfolio/vite.config.js
@@ -1,15 +1,27 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import sass from 'sass'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import sass from "sass";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  css: {
-    preprocessorOptions: {
-      scss: {
-        implementation: sass,
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [react()],
+    base: "/",
+    server: {
+      port: 3000,
+    },
+    css: {
+      preprocessorOptions: {
+        scss: {
+          implementation: sass,
+        },
       },
     },
-  },
-})
+  };
+
+  if (command !== "serve") {
+    config.base = "/Portfolio/";
+  }
+
+  return config;
+});

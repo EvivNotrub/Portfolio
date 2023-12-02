bab5adfd2a21aa1ac46274eb2a89ea1cdce2effe
EvivNotrub <barthwerle@yahoo.de>
2023-12-02 01:43:12 +0100

    nav more accesible & behavior on tabulation

--- Portfolio/src/components/buttons/mutton.jsx
+++ Portfolio/src/components/buttons/mutton.jsx
@@ -1,11 +1,20 @@
 import PropTypes from "prop-types";
 import "./mutton.scss";
 
-function Mutton({ addClass }) {
+function Mutton({ addClass, checked, setChecked }) {
   return (
-    <div className={"mutton" + " " + addClass}>
-      <input className="lines--sibling" type="checkbox" />
-      <button className="lines">
+    <div
+      className={"mutton" + " " + addClass}
+      onClick={(e) => e.stopPropagation()}
+    >
+      <input
+        className="lines--sibling"
+        type="checkbox"
+        onChange={() => setChecked(!checked)}
+        checked={checked}
+        value={checked}
+      />
+      <div className="lines">
         <div className="lines__center">
           <div className="lines__center__square"></div>
         </div>
@@ -13,13 +22,15 @@ function Mutton({ addClass }) {
         <div className="lines__line --two"></div>
         <div className="lines__line --three"></div>
         <div className="lines__line --four"></div>
-      </button>
+      </div>
     </div>
   );
 }
 
 Mutton.propTypes = {
   addClass: PropTypes.string,
+  checked: PropTypes.bool,
+  setChecked: PropTypes.func,
 };
 
 export default Mutton;
--- Portfolio/src/components/navigations/navigation.jsx
+++ Portfolio/src/components/navigations/navigation.jsx
@@ -1,15 +1,71 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import "./navigation.scss";
 import Mutton from "../buttons/mutton";
 
 function Navigation() {
+  const [checked, setChecked] = useState(false);
+  // on focus can be manged from line 8 to 13 in nav.scss
+  const [isFocused, setIsFocused] = useState(false);
+  const [subNav, setSubNav] = useState(false);
+
+  // closes the nav & sub-nav when the user clicks outside of the nav
+  // stopPropagation on Home link and inside Mutton component
+  useEffect(() => {
+    const closeNav = () => {
+      setChecked(false);
+      setIsFocused(false);
+      setSubNav(false);
+    };
+    window.addEventListener("click", closeNav);
+    return () => {
+      window.removeEventListener("click", closeNav);
+    };
+  }, []);
+
+  // closes the nav & sub-nav when the user hits the ENTER key or the ESC key
+  useEffect(() => {
+    const closeNav = (e) => {
+      if (e.keyCode === 13 || e.keyCode === 27) {
+        setChecked(false);
+        setIsFocused(false);
+        setSubNav(false);
+      }
+    };
+    window.addEventListener("keydown", closeNav);
+    return () => {
+      window.removeEventListener("keydown", closeNav);
+    };
+  }, []);
+
   return (
-    <nav data-testid="nav-testid" className="nav">
-      <Mutton addClass={"nav__mutton"} />
-      <ul className="nav__list">
+    <nav
+      onFocus={() => setIsFocused(true)}
+      onBlur={() => setIsFocused(false)}
+      data-testid="nav-testid"
+      className="nav"
+    >
+      <Mutton
+        addClass={"nav__mutton"}
+        checked={checked}
+        setChecked={setChecked}
+      />
+      <ul className={"nav__list" + " " + (checked || isFocused ? "show" : "")}>
         <li className="nav__list__item">
-          <Link to="/">Accueil</Link>
-          <nav data-testid="sub-nav-testid" className="sub-nav">
+          <Link
+            to="/"
+            onClick={(e) => e.stopPropagation()}
+            onFocus={() => setSubNav(true)}
+            onBlur={() => setSubNav(false)}
+          >
+            Accueil
+          </Link>
+          <nav
+            data-testid="sub-nav-testid"
+            className={"sub-nav" + " " + (subNav ? "show-sub" : "")}
+            onFocus={() => setSubNav(true)}
+            onBlur={() => setSubNav(false)}
+          >
             <ul className="sub-nav__list">
               <li className="sub-nav__item">
                 <Link data-testid="aboutLink" to="/#about">
@@ -25,7 +81,6 @@ function Navigation() {
             </ul>
           </nav>
         </li>
-
         <li className="nav__list__item">
           <Link to="/projects">Mes Projets</Link>
         </li>

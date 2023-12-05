c171e04e121ad02c2befde8562e020b7c405b1ac
EvivNotrub <barthwerle@yahoo.de>
2023-12-05 20:27:50 +0100

    update about section

--- Portfolio/src/containers/about/about.jsx
+++ Portfolio/src/containers/about/about.jsx
@@ -4,27 +4,30 @@ function About() {
   return (
     <div data-testid="about-testid" className="about">
       <h2 className="about__title">About</h2>
-      <p className="about__text">
-        This website is a showcase of the projects, skills and technologies I
-        worked on, they already provide a good deal of tools to create or
-        improve a website.
-      </p>
-      <p className="about__text">
-        This is just the beginning, more projects will come very soon, and
-        learning is part of everyday life.
-      </p>
-      <p className="about__text">
-        Fresh out of a developer program from Open Classrooms, the site is, as
-        of now, advertising some projects that are part of this degree. They are
-        made for the sole purpose of learning new skills and technologies, each
-        one with a specific topic. A description of the project provides context
-        and the goals we had, since not all of them were made from scratch. This
-        allows you to understand what was implemented by myself or not.
-      </p>
-      <p className="about__text">
-        Additional links to my resume, GitHub and others will provide more
-        information about myself.
-      </p>
+      <article className="about__article">
+        <p className="about__text">
+          This website is a showcase of the projects, skills and technologies I
+          worked on, they already provide a good deal of tools to create or
+          improve a website.
+        </p>
+        <p className="about__text">
+          This is just the beginning, more projects will come very soon, and
+          learning is part of everyday life.
+        </p>
+        <p className="about__text">
+          Fresh out of a developer program from Open Classrooms, the site is, as
+          of now, advertising some projects that are part of this degree. They
+          are made for the sole purpose of learning new skills and technologies,
+          each one with a specific topic. A description of the project provides
+          context and the goals we had, since not all of them were made from
+          scratch. This allows you to understand what was implemented by myself
+          or not.
+        </p>
+        <p className="about__text">
+          Additional links to my resume, GitHub and others will provide more
+          information about myself.
+        </p>
+      </article>
     </div>
   );
 }

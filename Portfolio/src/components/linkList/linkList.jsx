8eaa7031e181e5a000306e8289d754a2a0bb6dd3
EvivNotrub <barthwerle@yahoo.de>
2023-12-10 12:06:55 +0100

    missing list items in linkList and footer components

--- Portfolio/src/components/linkList/linkList.jsx
+++ Portfolio/src/components/linkList/linkList.jsx
@@ -1,12 +1,20 @@
 import { PropTypes } from "prop-types";
 import "./linkList.scss";
 
-function LinkList({ children }) {
-  return <ul className="link-list">{children}</ul>;
+function LinkList({ children, className }) {
+  console.log(children);
+  return (
+    <ul className={"link-list" + " " + (className && className)}>
+      {children.map((child, index) => {
+        return <li key={index}>{child}</li>;
+      })}
+    </ul>
+  );
 }
 
 LinkList.propTypes = {
   children: PropTypes.node,
+  className: PropTypes.string,
 };
 
 export default LinkList;

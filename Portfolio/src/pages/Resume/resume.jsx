4c0fa87c7c157b616419dbfa851775b31fef3f4c
EvivNotrub <barthwerle@yahoo.de>
2023-12-11 10:01:38 +0100

    resume page banner

--- Portfolio/src/pages/Resume/resume.jsx
+++ Portfolio/src/pages/Resume/resume.jsx
@@ -5,7 +5,9 @@ function Resume() {
 
   return (
     <main data-testid="resume-testid" className="resume__main">
-      <h1 className="resume__main__title">CV</h1>
+      <div className="resume__banner">
+        <h1 className="resume__main__title">CV</h1>
+      </div>
       <p className="resume__main__message">En cours de construction</p>
     </main>
   );

622e18bd36c6ef8d462a720798085bfd68908fb7
EvivNotrub <barthwerle@yahoo.de>
2024-03-14 00:23:28 +0100

    revert changes not working in crawler!

--- Portfolio/src/utils/helpers/helmet/helmetMetaData.jsx
+++ Portfolio/src/utils/helpers/helmet/helmetMetaData.jsx
@@ -3,7 +3,7 @@ import { PropTypes } from "prop-types";
 
 function HelmetMetaData({ title, description, ogtype, xtype, name, children }) {
   return (
-    <Helmet prioritizeSeoTags>
+    <Helmet>
       <title>{title}</title>
       <meta name="description" content={description} />
       <meta name="author" content={name} />

fad1178399edcea1e4af3134e4c6e57a10e0f510
EvivNotrub <barthwerle@yahoo.de>
2024-03-14 13:04:30 +0100

    Update metadata in index.html and helmetMetaData.jsx template dark image for meta

--- Portfolio/src/utils/helpers/helmet/helmetMetaData.jsx
+++ Portfolio/src/utils/helpers/helmet/helmetMetaData.jsx
@@ -1,19 +1,36 @@
-import { Helmet } from "react-helmet-async";
+import { Helmet } from "react-helmet";
 import { PropTypes } from "prop-types";
 
-function HelmetMetaData({ title, description, ogtype, xtype, name, children }) {
+function HelmetMetaData({
+  title,
+  description,
+  ogtype,
+  url,
+  xtype,
+  name,
+  img,
+  imgAlt,
+  children,
+}) {
   return (
-    <Helmet prioritizeSeoTags>
+    <Helmet>
       <title>{title}</title>
       <meta name="description" content={description} />
       <meta name="author" content={name} />
       <meta property="og:type" content={ogtype} />
       <meta property="og:title" content={title} />
       <meta property="og:description" content={description} />
+      <meta property="og:url" content={url} />
+      <meta property="og:site_name" content={title} />
+      <meta property="og:image" content={img} />
+      <meta property="og:image:alt" content={imgAlt} />
       <meta name="twitter:creator" content={name} />
       <meta name="twitter:card" content={xtype} />
       <meta name="twitter:title" content={title} />
       <meta name="twitter:description" content={description} />
+      <meta name="twitter:image" content={img} />
+      <meta name="twitter:image:alt" content={imgAlt} />
+
       {children}
     </Helmet>
   );
@@ -23,8 +40,11 @@ HelmetMetaData.propTypes = {
   title: PropTypes.string,
   description: PropTypes.string,
   ogtype: PropTypes.string,
+  url: PropTypes.string,
   xtype: PropTypes.string,
   name: PropTypes.string,
+  img: PropTypes.string,
+  imgAlt: PropTypes.string,
   children: PropTypes.node,
 };
 
